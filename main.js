3d6bab76e8cbe0f46f862dc153787b6bedf1a756
fred <daktarispacetime@gmail.com>
2022-12-22 22:01:36 +0300

    edit

--- main.js
+++ main.js
@@ -121,10 +121,34 @@ if (!data.current.is_day) {
 if (code == 1000) {
     //set BG to clear if weather is clear
 
-    app.getElementsByClassName.backgroundImage = 
+    app.style.backgroundImage = 
     `url(.images/${timeOfDay}/clear.jpg)`
+}
+
+btn.style.background = "#e5ba92";
+if(timeOfDay== "night") {
+    btn.style.background = "#181e27";
+}
 }
 
 app.lastElementChild.backgroundImage = 'url(./images/${timeOfDay}/cloudy.jpg)';
 
 btn.style.background = "#e5ba92";
+if(timeOfDay == "night") {
+    btn.style.background = "#181e27";
+}
+else if (
+    code == 1003 ||
+    code == 1006 ||
+    code == 1009 ||
+    code == 1030 ||
+    code == 1069 ||
+    code == 1087 ||
+    code == 1135 ||
+    code == 1273 ||
+    code == 1276 ||
+    code == 1279 ||
+    code == 1282
+){
+    
+}
